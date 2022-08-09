f01ec3d666fbb348595b402b7c6e513a5c749927
yusbaklee <67943334+yusbaklee@users.noreply.github.com>
2022-08-09 08:54:01 +0100

    Add project files.

---
+++ 4by4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace assignment_matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+                {
+                { 0, 0, 0, 0},
+                { 8, 1, 5, 3 },
+                { 0, -1, 0, 0 },
+                { 5, 3, 1, 1 }};
+
+            int det = (matrix[0, 0] * ((matrix[1, 1] * ((matrix[2, 2] * matrix[3, 3]) - (matrix[3, 2] * matrix[2, 3])) - (matrix[1, 2] * ((matrix[2, 1] * matrix[3, 3]) - (matrix[3, 1] * matrix[2, 3])) + (matrix[1, 3] * ((matrix[2, 1] * matrix[3, 2]) - (matrix[3, 1] * matrix[2, 2]))))))) -
+                (matrix[0, 1] * ((matrix[1, 0] * ((matrix[2, 2] * matrix[3, 3]) - (matrix[3, 2] * matrix[2, 3])) - (matrix[1, 2] * ((matrix[2, 0] * matrix[3, 3]) - (matrix[3, 0] * matrix[2, 3])) + (matrix[1, 3] * ((matrix[2, 0] * matrix[3, 2]) - (matrix[3, 0] * matrix[2, 2]))))))) +
+                (matrix[0, 2] * ((matrix[1, 0] * ((matrix[2, 1] * matrix[3, 3]) - (matrix[3, 1] * matrix[2, 3])) - (matrix[1, 1] * ((matrix[2, 0] * matrix[3, 3]) - (matrix[3, 0] * matrix[2, 3])) + (matrix[1, 3] * ((matrix[2, 0] * matrix[3, 1]) - (matrix[3, 0] * matrix[2, 1]))))))) -
+                (matrix[0, 3] * ((matrix[1, 0] * ((matrix[2, 1] * matrix[3, 2]) - (matrix[3, 1] * matrix[2, 2])) - (matrix[1, 1] * ((matrix[2, 0] * matrix[3, 2]) - (matrix[3, 0] * matrix[2, 2])) + (matrix[1, 2] * ((matrix[2, 0] * matrix[3, 1]) - (matrix[3, 0] * matrix[2, 1])))))));
+
+            Console.WriteLine(det);
+        }
+    }
+}
